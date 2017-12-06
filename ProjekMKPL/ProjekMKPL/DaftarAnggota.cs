50e872890cd52469a30788bc29ccd00b8e450ed6
awns-ida <siti.wahidah96@gmail.com>
2017-12-06 12:43:00 +0700

    query DaftarAnggota dan TambahAnggota

--- ProjekMKPL/ProjekMKPL/DaftarAnggota.cs
+++ ProjekMKPL/ProjekMKPL/DaftarAnggota.cs
@@ -16,9 +16,69 @@ namespace ProjekMKPL
         public DaftarAnggota()
         {
             InitializeComponent();
+            this.refresh_data_anggota();
 
         }
-        
-        
+
+        public void refresh_data_anggota()
+        {
+            MySqlConnection conn = this.makeDatabaseConnection();
+
+            try
+            {
+                conn.Open();
+
+                String sql =
+                    "SELECT * " +
+                    "FROM anggota ";
+
+                MySqlCommand command = new MySqlCommand(sql, conn);
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+
+                this.dataGridViewAnggota.DataSource = dt;
+                conn.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        public void show_form()
+        {
+            this.Show();
+        }
+
+        public MySqlConnection makeDatabaseConnection()
+        {
+            MySqlConnection connection = new MySqlConnection();
+            String connString =
+                "Server=127.0.0.1;" +
+                "uid=root;" +
+                "pwd=;" +
+                "database=perpus";
+
+            connection.ConnectionString = connString;
+
+            return connection;
+        }
+
+
+        private void DaftarAnggota_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewAnggota_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
--- ProjekMKPL/ProjekMKPL/tambahAnggota.Designer.cs
+++ ProjekMKPL/ProjekMKPL/tambahAnggota.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(tambahAnggota));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -45,8 +44,6 @@
             this.textBoxAlamat = new System.Windows.Forms.TextBox();
             this.buttonSimpan = new System.Windows.Forms.Button();
             this.buttonBatal = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -176,6 +173,7 @@
             this.buttonSimpan.TabIndex = 14;
             this.buttonSimpan.Text = "Simpan";
             this.buttonSimpan.UseVisualStyleBackColor = true;
+            this.buttonSimpan.Click += new System.EventHandler(this.buttonSimpan_Click);
             // 
             // buttonBatal
             // 
@@ -185,16 +183,7 @@
             this.buttonBatal.TabIndex = 15;
             this.buttonBatal.Text = "Batal";
             this.buttonBatal.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(417, 11);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(33, 34);
-            this.pictureBox1.TabIndex = 16;
-            this.pictureBox1.TabStop = false;
+            this.buttonBatal.Click += new System.EventHandler(this.buttonBatal_Click);
             // 
             // tambahAnggota
             // 
@@ -202,7 +191,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.IndianRed;
             this.ClientSize = new System.Drawing.Size(462, 322);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.buttonBatal);
             this.Controls.Add(this.buttonSimpan);
             this.Controls.Add(this.textBoxAlamat);
@@ -221,7 +209,6 @@
             this.Controls.Add(this.label1);
             this.Name = "tambahAnggota";
             this.Text = "7";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -245,6 +232,5 @@
         private System.Windows.Forms.TextBox textBoxAlamat;
         private System.Windows.Forms.Button buttonSimpan;
         private System.Windows.Forms.Button buttonBatal;
-        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
